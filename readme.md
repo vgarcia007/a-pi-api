bd2a8a8f5c5ad09f50da6cc6f297225f2feb759a
Philipp <philipp@freilinger.org>
2020-09-13 20:25:16 +0200

    change service name

--- readme.md
+++ readme.md
@@ -2,5 +2,7 @@
 
 Installation
 ```
+git clone https://github.com/vgarcia007/a-pi-api.git
+cd a-pi-api
 sudo /bin/bash setup.sh
 ```
--- setup.sh
+++ setup.sh
@@ -14,13 +14,13 @@ source ./bin/activate
 pip3 install -r requirements.txt
 cd ..
 #stop service in case its allready installed
-systemctl stop rpi_api
+systemctl stop a_pi_api
 #create .service file
 sed "s|__APP_DIR__|$current_working_dir|g" <a_pi_api.template.service >a_pi_api.service
-mv rpi_api.service /etc/systemd/system/rpi_api.service
+mv a_pi_api.service /etc/systemd/system/a_pi_api.service
 #lets go
 systemctl daemon-reload
-systemctl start rpi_api
-systemctl status rpi_api
+systemctl start a_pi_api
+systemctl status a_pi_api
 echo "use systemctl to controll the service"
 echo "you can reach the api at http://%RASPBERRY_IP_ADDRESS:8000"
